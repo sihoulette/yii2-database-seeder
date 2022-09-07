1694b682264032332efafbe90c6dd87bc482e388
Bytsyk Aleksandr <aleksandr.bytsyk@gmail.com>
2022-09-07 23:29:26 +0300

    Oops, web config load dotenv bug fix.

--- web/index.php
+++ web/index.php
@@ -5,6 +5,14 @@ defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 // Composer autoload
 require dirname(__DIR__) . DS . 'vendor' . DS . 'autoload.php';
 
+// Symfony Dotenv load
+try {
+    $dotenv = new \Symfony\Component\Dotenv\Dotenv();
+    $dotenv->load(dirname(__DIR__) . DS . '.env');
+} catch (\Exception $e) {
+
+}
+
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', env('YII_DEBUG', true));
 defined('YII_ENV') or define('YII_ENV', env('YII_ENV', 'dev'));
